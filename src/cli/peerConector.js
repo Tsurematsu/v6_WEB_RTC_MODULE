9390f5e5ecf96a72145609269453a8baa54e0bd5
Tsurematsu <daniel120802@gmail.com>
2024-04-24 11:05:31 -0500

    conneccion

--- src/cli/peerConector.js
+++ src/cli/peerConector.js
@@ -1,115 +1,86 @@
-import socket from "./socket.js";
 import { ReadJSON, log, showAlert } from "./toolkit.js";
 // log.hidden = ["system", "peer", "chatPeer"];
-// log.hidden = ["test"]
 
-export default async function peerConector({
-    url="/", 
-    newMember=()=>{}, 
-    name="Juan", 
-    room="room1"
-}){
-    url = String(url);
-    name = String(name);
-    room = String(room);
-    
-    let Socket = await socket({url, newMember, name, room});
-    
+export default peerConector;
+
+async function peerConector({ url="/", name="Juan", room="room1"}){
+    url = String(url); name = String(name); room = String(room);
     const iceServers = await ReadJSON("/cli/iceServers.json");
     const peerClient = new RTCPeerConnection({iceServers});
-    RTC_CHAT(peerClient);
-    
-    
-    Socket.Events.join(async (entry, retorno, sender)=>{
-        log.id("My id:", Socket.id);
-        peerClient.createOffer().then((offer) => {
-            peerClient.setLocalDescription(offer);
-            peerClient.onicecandidate = (event) => {
-                if (event.candidate) {
-                    log.candidates("Enviando candidato a", sender);
-                    Socket.Emit("candidate", event.candidate, sender); 
-                }
-            };
-            
-            // Socket.Emit("offer", offer, sender);
-            retorno("offer", offer);
-        });
-    })
-
-    Socket.Events.offer(async (entry, retorno, sender)=>{
-        await peerClient.setRemoteDescription(new RTCSessionDescription(entry))
-        const answer = await peerClient.createAnswer();
-        peerClient.setLocalDescription(answer);
-        retorno("answer", answer);
-    })
-    
-    Socket.Events.answer(async (entry, _, sender)=>{
-        await peerClient.setRemoteDescription(new RTCSessionDescription(entry))
-    })
-
-    Socket.Events.candidate((entry, _, sender)=>{
-        log.candidates("Recibiendo candidato de", sender);
-        peerClient.addIceCandidate(new RTCIceCandidate(entry));
-    })
-    
-   
-    
-    
-}
-
-
-
-
-
-let sendChat = ()=>"";
-let sendChat1 = ()=>"";
-async function RTC_CHAT(peerClient){
     const chat = peerClient.createDataChannel('chat');
     chat.addEventListener('open', () => {
-        log.chatPeer("Chat open");
-        sendChat = (msg) => chat.send(msg);
+        log.chatPeer("connection establecida!!");
     });
-    const chat1 = peerClient.createDataChannel('canal1');
-    chat1.addEventListener('open', () => {
-        log.chatPeer("Chat 1 open");
-        sendChat1 = (msg) => chat1.send(msg);
+    // const io = (await import("./socket.io.esm.min.js")).default;
+    
+    const socket = io(url);
+    let countConnected = 0;
+    socket.on("connect", () => {
+        countConnected++; if (countConnected>=2) {location.reload();}
+        log.system("connect", socket.id);
+        socket.on("join", (entry) => {
+            peerClient.onicecandidate = (event) => {
+                if (event.candidate) {
+                    log.candidates("Enviando candidato");
+                    socket.emit("candidate", {room, data:event.candidate}); 
+                }
+            };
+            peerClient.createOffer().then((offer) => {
+                socket.on("answer", (answer) => {
+                    log.system("Recibiendo answer");
+                    peerClient.setRemoteDescription(new RTCSessionDescription(answer));
+                });
+
+                peerClient.setLocalDescription(offer);
+                log.system("Enviando offer");
+                socket.emit("offer", {room, data:offer});
+            });
+        });
+        socket.emit("join", {room});
+        socket.on("offer", async (entry) => {
+            peerClient.setRemoteDescription(new RTCSessionDescription(entry))
+            peerClient.createAnswer().then((answer) => {
+                log.system("answer creado");
+                peerClient.setLocalDescription(answer);
+                log.system("Enviando answer");
+                socket.emit("answer", {room, data:answer});
+            });
+        });
+        socket.on("candidate", (entry) => {
+            log.candidates("Recibiendo candidato");
+            peerClient.addIceCandidate(new RTCIceCandidate(entry));
+        });
     });
-    peerClient.addEventListener('datachannel', (event) => {
-        const canal = event.channel;
-        if (canal.label == 'chat') {
-            log.chatPeer("Chat message:", canal);     
-        }
-        if (canal.label == 'canal1') {
-            log.chatPeer("Chat 1 message:", canal);
-        }
-    });
+
+
+
+    return;
+    // const peerClient = new RTCPeerConnection({iceServers});
+    // RTC_CHAT(peerClient);
+    
+    // let sendChat = ()=>"";
+    // let sendChat1 = ()=>"";
+    // async function RTC_CHAT(peerClient){
+    //     const chat = peerClient.createDataChannel('chat');
+    //     chat.addEventListener('open', () => {
+    //         log.chatPeer("Chat open");
+    //         sendChat = (msg) => chat.send(msg);
+    //     });
+    //     const chat1 = peerClient.createDataChannel('canal1');
+    //     chat1.addEventListener('open', () => {
+    //         log.chatPeer("Chat 1 open");
+    //         sendChat1 = (msg) => chat1.send(msg);
+    //     });
+    //     peerClient.addEventListener('datachannel', (event) => {
+    //         const canal = event.channel;
+    //         if (canal.label == 'chat') {
+    //             log.chatPeer("Chat message:", canal);     
+    //         }
+    //         if (canal.label == 'canal1') {
+    //             log.chatPeer("Chat 1 message:", canal);
+    //         }
+    //     });
+    // }
 }
 
 
-
-
-
-// saved Code
-
-
-  // Socket.events.offer(async (data) => {
-    //     if (log.test("OFFER MEMBER: ", data)) {
-    //         Socket.emit.answer(data.id, {data: "Hola soy el cliente 2"});
-    //     }else{
-    //         // await showAlert(`Oferta del miembro`);
-    //         // log.peer("Receive offer from:", data.id);
-    //         // await peerClient.setRemoteDescription(new RTCSessionDescription(data.msg))
-    //         // const answer = await peerClient.createAnswer();
-    //         // peerClient.setLocalDescription(answer);
-    //         // Socket.sendMessage('answer', {client:data.id, data:answer});
-    //     }
-    // });
-
-    // Socket.events.answer(async (data) => {
-    //     if (!log.test("ANSWER MEMBER: ", data)) {
-    //         log.peer("Receive answer from:", data.id);
-    //         await showAlert(`Respuesta del miembro`);
-    //         await peerClient.setRemoteDescription(new RTCSessionDescription(data.msg))
-    //     }
-    // });
-    
--- src/libs/Socket/Socket.js
+++ src/libs/Socket/Socket.js
@@ -4,45 +4,27 @@ export default function Socket(server) {
     const rooms = {};
     const rooms_socket = {};
 
-    const Members = (room) => Object.keys(rooms).filter((id) => rooms[id].room == room);
-    const RoomClient = (id) => rooms[id].room;
     socket.on('connection', (clientConnected) => {
         clientConnected.on('join', (entry) => {
-          const room = String(entry.room??entry);
+          const room = String(entry.room??entry).trim();
           clientConnected.join(room);
-          rooms[clientConnected.id] = { id: clientConnected.id, room};
-          rooms_socket[clientConnected.id] = clientConnected;
-          socket.to(room).emit('join', {sender: clientConnected.id, members: Members(room)});
-        });       
-
+          clientConnected.to(room).emit('join', {sender: clientConnected.id});
+        });   
+        
         clientConnected.on('offer', (entry) => {
-          // if (entry.addresses !== null && rooms_socket[entry.addresses] !== undefined) {
-          //   rooms_socket[entry.addresses].emit('offer', {sender: clientConnected.id, data: entry.data});
-          // }
-          socket.to(RoomClient(clientConnected.id)).emit('offer', {sender: clientConnected.id, data: entry.data});
+          clientConnected.to(entry.room).emit('offer', entry.data);
         });
 
         clientConnected.on('answer', (entry) => {
-          // if (entry.addresses !== null && rooms_socket[entry.addresses] !== undefined) {
-          //   rooms_socket[entry.addresses].emit('answer', {sender: clientConnected.id, data: entry.data});
-          // }
-          socket.to(RoomClient(clientConnected.id)).emit('answer', {sender: clientConnected.id, data: entry.data});
+          clientConnected.to(entry.room).emit('answer', entry.data);
         });
 
         clientConnected.on('candidate', (entry) => {
-          // if (entry.addresses !== null && rooms_socket[entry.addresses] !== undefined) {
-          //   rooms_socket[entry.addresses].emit('candidate', {sender: clientConnected.id, data: entry.data});
-          // }
-          socket.to(RoomClient(clientConnected.id)).emit('candidate', {sender: clientConnected.id, data: entry.data});
+          clientConnected.to(entry.room).emit('candidate', entry.data);
         });
 
         clientConnected.on('disconnect', async () => {
-          if (rooms[clientConnected.id] != undefined) {
-            let room = RoomClient(clientConnected.id);
-            delete rooms[clientConnected.id];
-            delete rooms_socket[clientConnected.id];
-            socket.emit('leave', {id: clientConnected.id, members:Members(room)});
-          }
+          clientConnected.emit('leave', {id: clientConnected.id});
         });
         
     });
